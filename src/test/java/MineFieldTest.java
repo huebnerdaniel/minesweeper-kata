526c57df7a029b04914c3709a834ffbb90617802
daniel.huebner <daniel.huebner@idealo.de>
2020-09-01 08:10:54 +0200

    Initial maven setup and first failing test

---
+++ src/test/java/MineFieldTest.java
@@ -0,0 +1,11 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class MineFieldTest {
+
+    @Test
+    void start_here() {
+        assertThat(true).isFalse();
+    }
+}
